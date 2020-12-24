072a8903976e43d2b3b5979b6d0dd82ab12edb5b
sandeep <sandeep@knowledgelens.com>
2020-12-24 12:28:18 +0530

    First commit

---
+++ TestProfile/TestProfile/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  TestProfile
+//
+//  Created by sandeep kumar on 24/12/20.
+//
+
+import Foundation
+public class Profile{
+    //private init() {}
+    public func tetString()->String{
+        return UUID().uuidString
+    }
+}
---
+++ TestProfile/TestProfile/TestProfile.h
@@ -0,0 +1,18 @@
+//
+//  TestProfile.h
+//  TestProfile
+//
+//  Created by sandeep kumar on 24/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestProfile.
+FOUNDATION_EXPORT double TestProfileVersionNumber;
+
+//! Project version string for TestProfile.
+FOUNDATION_EXPORT const unsigned char TestProfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestProfile/PublicHeader.h>
+
+
